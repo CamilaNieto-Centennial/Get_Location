174c9286c043f2ba14c27008a286a2b2f79e0091
CamilaNieto-Centennial <fourcamilitanieto@gmail.com>
2024-02-13 17:22:57 -0600

    feat: Favicon Added & Logo Added on Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@ export const Navbar = () => {
     return (
         <nav className="bg-primary py-4">
             <div className="container mx-auto px-4">
-                <h1 className="text-white text-2xl font-semibold">My Location App</h1>
+                <img src="./white-logo.png" alt="Logo" className="size-1/4" />
             </div>
         </nav>
     );
